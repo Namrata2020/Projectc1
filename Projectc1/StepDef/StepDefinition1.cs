4039fb19cdc752e73fa0b4027b33fea887c78ea8
Namrata2020 <namratajagtap2020@gmail.com>
2020-07-30 22:10:58 +0530

    Add project files.

---
+++ Projectc1/StepDef/StepDefinition1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace Projectc1.StepDef
+{
+    [Binding]
+    public sealed class StepDefinition1
+    {
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            Console.WriteLine(p0);
+        }
+
+        [Given(@"the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int p0)
+        {
+            
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            
+        }
+
+    }
+}
